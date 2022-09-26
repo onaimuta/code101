ab43d7120725b678871e5c02ec21a13271627ab2
Onai Hector <onaimuta@gmail.com>
2022-09-26 09:13:43 +0300

    Sunday Commits

---
+++ Python/hackerank_tests.py
@@ -0,0 +1,10 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+a = 12 # Declare second integer, double, and String variables.
+b = 4.0 # Read and save an integer, double, and String to your variables.
+print (a + i) # Print the sum of both integer variables on a new line.
+print (d + b) # Print the sum of the double variables on a new line.
+c =  ("is the best place to learn and practice coding!") # Concatenate and print the String variables on a new line
+print (s + c) # The 's' variable above should be printed first.
+
