d36c5de56ac5500562d23dfd3a84dcffe7a9fafd
Onai Hector <onaimuta@gmail.com>
2022-09-29 16:53:28 +0300

    290922

---
+++ Python/freecodecamp_playground.py
@@ -0,0 +1,48 @@
+# largest_so_far = -1
+# print ("Before", largest_so_far)
+# for the_num in [9, 41, 12, 3, 74, 15, 78] :
+#     if the_num > largest_so_far: 
+#         largest_so_far = the_num
+#     print (largest_so_far, the_num)
+# print("After", largest_so_far)
+
+# smallest = None
+# print ("Before", smallest)
+# for itervar in [3, 41, 12, 9, 74, 15] :
+#     if smallest is None or itervar < smallest:
+#         smallest = itervar
+#         break
+#     print ("Loop:", itervar, smallest)
+# print ("Smallest", smallest)
+
+# zork = 0
+# print ("Before", zork)
+# for thing in [3, 41, 12, 9, 74, 15] :
+#     zork = zork + thing
+#     print (zork, thing)
+# print("After", zork)
+
+fruit = "banana"
+letter = fruit[1]
+print  (letter)
+print ("-----------------------------------------")
+x = 3
+w = fruit [x-1]
+print(w)
+print ("-----------------------------------------")
+# zot = "abc"
+# print(zot[5])
+
+print(len(fruit))
+print ("-----------------------------------------")
+for n in "banana":
+    print(n)
+print ("-----------------------------------------")
+
+word = "bananana"
+#i = word.find("na")
+
+dict = {"Fri": 20, "Thu": 6, "Sat": 1}
+dict["Thu"] = 13
+dict["Sat"] = 2
+dict["Sun"] = 9
