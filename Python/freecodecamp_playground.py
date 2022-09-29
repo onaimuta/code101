12b4430830796da881150c2601097d7718aa3057
Onai Hector <onaimuta@gmail.com>
2022-09-29 19:22:16 +0300

    word count function

--- Python/freecodecamp_playground.py
+++ Python/freecodecamp_playground.py
@@ -33,16 +33,20 @@ print ("-----------------------------------------")
 # zot = "abc"
 # print(zot[5])
 
-print(len(fruit))
-print ("-----------------------------------------")
-for n in "banana":
-    print(n)
-print ("-----------------------------------------")
+# print(len(fruit))
+# print ("-----------------------------------------")
+# for n in "banana":
+#     print(n)
+# print ("-----------------------------------------")
 
-word = "bananana"
-#i = word.find("na")
+# word = "bananana"
+# #i = word.find("na")
 
-dict = {"Fri": 20, "Thu": 6, "Sat": 1}
-dict["Thu"] = 13
-dict["Sat"] = 2
-dict["Sun"] = 9
+# dict = {"Fri": 20, "Thu": 6, "Sat": 1}
+# dict["Thu"] = 13
+# dict["Sat"] = 2
+# dict["Sun"] = 9
+
+text = ("I am Systems Administrator turned DevOps Engineer, I am dedicated Technology Specialist with vast hands on experience in the areas of DevOps, Systems Administration, IT Consultation and Systems Engineering. I have more than 10 years experience working in Tech both as an employee and Freelancer.  I am conversant with Linux and Windows Systems, very good with troubleshooting different technologies on the Market. I have designed, built networks and managed Networks in my career. I can carry out Hardware and Software Maintenance, Upgrades and Deployments for any computer environment. Below is a summary of my skills: DevOps, Linux, Windows, Bash, Python, JSON, YAML, Git, Git-Hub, Cloud, IAM, AWS, S3, EC2, Slack, HTML, Shell-Scripting, CLI, Google Suite, Microsoft-365, MS-Office, Active-Directory, Server-Administration, Network-Administration, Cabling, TCP/IP, DHCP, DNS, Hardware, Tech-Support, MS Azure, VoIP, Virtualization, Containers, Microservices, Agile, Monitoring, Troubleshooting, Storage, Infrastructure-Deployment. Docker, Docker Hub, Jenkins, Terraform, Ansible, Kubernetes, CI/CD Pipelines, Git Actions, SQL.")
+print(len(text.split()))
+
