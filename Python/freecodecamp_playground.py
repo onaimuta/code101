4f39ab44e723cfac2ff0ac6729aebb23566cfda7
Onai Hector <onaimuta@gmail.com>
2022-09-30 04:56:28 +0300

    Lists vs Tuples

--- Python/freecodecamp_playground.py
+++ Python/freecodecamp_playground.py
@@ -1,52 +1,51 @@
-# largest_so_far = -1
-# print ("Before", largest_so_far)
-# for the_num in [9, 41, 12, 3, 74, 15, 78] :
-#     if the_num > largest_so_far: 
-#         largest_so_far = the_num
-#     print (largest_so_far, the_num)
-# print("After", largest_so_far)
+# import random
+# def getAnswer(answerNumber):
+#     if answerNumber == 1:
+#         return "Its is certain"
+#     elif answerNumber == 2:
+#         return "Its is decidedly so"
+#     elif answerNumber == 3:
+#         return "Yes"
+#     elif answerNumber == 4:
+#         return 'Reply hazy try again'
+#     elif answerNumber == 5:
+#         return 'Ask again later'
+#     elif answerNumber == 6:
+#         return 'Concentrate and ask again'
+#     elif answerNumber == 7:
+#         return 'My reply is no'
+#     elif answerNumber == 8:
+#         return 'Outlook not so good'
+#     elif answerNumber == 9:
+#         return 'Very doubtful'
+#     elif answerNumber == None:
+#         return "Spam"
 
-# smallest = None
-# print ("Before", smallest)
-# for itervar in [3, 41, 12, 9, 74, 15] :
-#     if smallest is None or itervar < smallest:
-#         smallest = itervar
-#         break
-#     print ("Loop:", itervar, smallest)
-# print ("Smallest", smallest)
+# r = random.randint(1, 9)
+# fortune = getAnswer(r)
+# print(fortune)
+import sys
+feline = ["cats", "lions", "cheetahs", "leopards", "jaguars", "tigers"]
+bovine = ["cattle", "bisons", "cape buffalos", "water buffalos", "spiral-horned antelopes"]
+#print (feline [0])
+print ("The " + feline [1] + " ate the " +bovine [2])
+print (bovine.index ("water buffalos"))
+bovine.insert (0, "eland")
+# if "water buffalos" in bovine:
+#     print("The " + bovine[0] + " are eaten by "+ feline[1]+".")
+#    sys.exit()
+del feline [0]
+del bovine [5]
+    # print ("Thats it!")
+bovine.sort()
+feline.sort()
+# for a in range(5):
+#     print(a)
 
-# zork = 0
-# print ("Before", zork)
-# for thing in [3, 41, 12, 9, 74, 15] :
-#     zork = zork + thing
-#     print (zork, thing)
-# print("After", zork)
+print (bovine)
+print (feline)
 
-fruit = "banana"
-letter = fruit[1]
-print  (letter)
-print ("-----------------------------------------")
-x = 3
-w = fruit [x-1]
-print(w)
-print ("-----------------------------------------")
-# zot = "abc"
-# print(zot[5])
 
-# print(len(fruit))
-# print ("-----------------------------------------")
-# for n in "banana":
-#     print(n)
-# print ("-----------------------------------------")
 
-# word = "bananana"
-# #i = word.find("na")
 
-# dict = {"Fri": 20, "Thu": 6, "Sat": 1}
-# dict["Thu"] = 13
-# dict["Sat"] = 2
-# dict["Sun"] = 9
-
-text = ("I am Systems Administrator turned DevOps Engineer, I am dedicated Technology Specialist with vast hands on experience in the areas of DevOps, Systems Administration, IT Consultation and Systems Engineering. I have more than 10 years experience working in Tech both as an employee and Freelancer.  I am conversant with Linux and Windows Systems, very good with troubleshooting different technologies on the Market. I have designed, built networks and managed Networks in my career. I can carry out Hardware and Software Maintenance, Upgrades and Deployments for any computer environment. Below is a summary of my skills: DevOps, Linux, Windows, Bash, Python, JSON, YAML, Git, Git-Hub, Cloud, IAM, AWS, S3, EC2, Slack, HTML, Shell-Scripting, CLI, Google Suite, Microsoft-365, MS-Office, Active-Directory, Server-Administration, Network-Administration, Cabling, TCP/IP, DHCP, DNS, Hardware, Tech-Support, MS Azure, VoIP, Virtualization, Containers, Microservices, Agile, Monitoring, Troubleshooting, Storage, Infrastructure-Deployment. Docker, Docker Hub, Jenkins, Terraform, Ansible, Kubernetes, CI/CD Pipelines, Git Actions, SQL.")
-print(len(text.split()))
 
--- Python/testbench.py
+++ Python/testbench.py
@@ -1,13 +1,33 @@
-import datetime
-import random, sys, os, math
-from random import *
-for i in range(5):
-    sys.exit()
-    print (random.randint(1, 10))
 
-while True:
-    print('Type exit to exit.')
-    response = input()
-    if response == 'exit':
-        sys.exit()
-    print('You typed ' + response + '.')
+# import datetime
+# import random, sys, os, math
+# from random import *
+# for i in range(5):
+#     sys.exit()
+#     print (random.randint(1, 10))
+
+# while True:
+#     print('Type exit to exit.')
+#     response = input()
+#     if response == 'exit':
+#         sys.exit()
+#     print('You typed ' + response + '.')
+
+print ("Slang Greetings in Zimbabwe")
+print ("Shona")
+print (("---------------------------------------"))
+def shona_greeting ():
+    print ("Wangu!")
+    print ("Kuricei")
+shona_greeting()
+print ("---------------------------------------")
+print ("Ndebele")
+print ("---------------------------------------")
+def ndebele_greeting ():
+    print ("Etha")
+    print("Zikhuphani")
+ndebele_greeting()
+
+# def funtion (argument)                             
+
+
