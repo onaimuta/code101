ea47813b3e86e0460d40ee5f032011c7a3c212e2
Onai Hector <onaimuta@gmail.com>
2022-10-06 10:45:06 +0300

    updates

--- Bash/time.sh
+++ Bash/time.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-
 timenow=$(date "+%T")
 datetoday=$(date +"%m-%d-%y")
 timezone=$(date +"%Z")
-
 echo -n "The date is: "
-sleep 1
-
-#banner also displays text
-#figlet command also displays text
-
+# sleep 1
+# banner also displays text
+# figlet command also displays text
+echo "$line"
 toilet $datetoday
-sleep 1
-
+# sleep 1
+echo "$line"
 echo -n "The time is: "
-sleep 1
-
+# sleep 1
+echo "$line"
 toilet $timenow
-sleep 1
-
+# sleep 1
+echo "$line"
 echo -n "The time zone is: "
-
+echo "$line"
 toilet $timezone
-
-exit
+echo "$line"
