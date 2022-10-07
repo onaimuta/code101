4337650393167cb38194ccc37b826718910f634d
Onai Hector <onaimuta@gmail.com>
2022-10-07 18:38:36 +0300

    Clean Up.

--- Python/TestPage.py
+++
@@ -1,8 +0,0 @@
-# Test Commands
-print(bool("abc"))
-
-print(bool(0))
-
-fruits = ["apple", "banana"]
-if "apple" in fruits:
-    print("Yes, apple is a fruit!")
--- Python/test.py
+++
@@ -1,9 +0,0 @@
-n = 0
-while True:
-    if n == 3:
-        break
-    print(n)
-    n = n + 1
-    
-for i in [2,1,5]:
-    print(i)
