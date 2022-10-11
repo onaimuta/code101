07f1b3ec3277ab95847215e481f2a5d0e8c35878
Onai Hector <onaimuta@gmail.com>
2022-10-11 11:39:42 +0300

    get system info code

--- Python/get_system_info.py
+++ Python/get_system_info.py
@@ -1,18 +1,18 @@
 from datetime import datetime
-
 now = datetime.now()
 print ("The day is: ", now.strftime("%d-%m-%y"))
-print (************************************************.)
+print ("********************************************************")
 # current_time = now.strftime("%H:%M:%S")
 # print("Current Time =", current_time)
-print (-------------------------------------------------)
 from concurrent.futures.process import _check_system_limits
-import os, sys, shutil, platform, subprocess, json, yaml, pandas
+import os, sys, shutil, platform, subprocess, json, yaml, pandas, socket
 from socket import socket
-print (-------------------------------------------------)
-
-print ("The current working dir is: ", os.getcwd())
+print ("--------------------------------------------------------")
+# print ("The current working dir is: ", os.getcwd())
 print ("No. of CPU cores is: ",os.cpu_count())
 print ("The logged on user is: ", os.getlogin())
 print ("The machine Architecture is: ", platform.architecture())
 print ("The current CPU is: ", platform.processor())
+print ("The OS is: ", platform.system())
+print ("The hostname is: ", platform.node())
+print("--------------------- The End ---------------------------")
