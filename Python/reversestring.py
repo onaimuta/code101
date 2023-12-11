27698f63f8052f4462e7aac28ec7f440d7d56a91
Onai Hector <onaimuta@gmail.com>
2023-12-11 19:13:41 +0300

    Reverse String

--- Python/reversestring.py
+++ Python/reversestring.py
@@ -2,14 +2,5 @@
 string = input("Enter the string: ")
 string = string[::-1]       #Much faster than any other method for reversing a string. 
 print ("Reversed string : "+ string)
-<<<<<<< HEAD
-<<<<<<< HEAD
-input ("Press Enter to Exit.")
 input ("Press Enter to Exit.")
 
-=======
-input ("Press Enter to Exit.")
->>>>>>> code102
-=======
-input ("Press Enter to Exit.")
->>>>>>> code102
