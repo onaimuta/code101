cb3f66c786711642f29976a3b8860ceab88a0815
Onai Hector <onaimuta@gmail.com>
2023-12-15 11:19:00 +0300

    os name tool

--- Python/os_module.py
+++ Python/os_module.py
@@ -1,14 +1,3 @@
 import os
 print ("Built on "+os.name+" technology")
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-# print (os.environ)
-# print (os.environ)
-
-=======
-# print (os.environ)
->>>>>>> code102
-=======
-# print (os.environ)
->>>>>>> code102
+print (os.environ)
